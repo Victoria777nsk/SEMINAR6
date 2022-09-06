403863ad5fb64f917cf8db12dd957ada29835b5c
Victoria <victoriaerygina@yandex.ru>
2022-09-06 15:07:15 +0700

    Второй метод перевода числа в двоичную систему

---
+++ Задача_42_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool[] array = Num2(num);
+
+Console.WriteLine(String.Join("", array));
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(Convert.ToInt32(array[i]));
+}
+
+bool[] Num2 (int num) // bool равносильно 1 БИТу
+{
+    int result = 0;
+    bool[] arr = new bool[32]; // размерность массива = 32 бита, т.к. введеное число int занимает 32 бита.
+    int i;
+    for (i = 0; num > 0; i++)
+    {
+        result = num % 2;
+        num = num / 2;
+        arr[arr.Length -1 -i] = Convert.ToBoolean(result);
+    }
+    return arr;
+}
