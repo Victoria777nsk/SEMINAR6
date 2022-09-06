d2e60c44e0f9dfa87340e99308632a6b1dd8e18a
Victoria <victoriaerygina@yandex.ru>
2022-09-06 13:45:04 +0700

    Неравенство сторон треугольника

---
+++ Задача_40/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник со сторонами такой длины.
+// (Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон).
+
+void Triangle ()
+{
+    Console.Write("Введите число 1: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите число 2: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите число 3: ");
+    int number3 = Convert.ToInt32(Console.ReadLine());
+
+    if (number1 < number2 + number3 && number2 < number1 + number3 && number3 < number2 + number1)
+    {
+        Console.WriteLine($"Теугольник со сторонами: {number1}, {number2} и {number3} МОЖЕТ существовать");
+    }
+    else
+    {
+        Console.WriteLine($"Теугольник со сторонами: {number1}, {number2} и {number3} НЕ МОЖЕТ существовать");
+    }
+}
+Triangle();
