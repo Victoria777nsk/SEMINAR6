32f083beadd71742df7f5937a688cec2b5c62986
Victoria <victoriaerygina@yandex.ru>
2022-09-06 14:35:04 +0700

    Перевод десятичного числа в двоичную систему

---
+++ Задача_42/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int value = number;
+
+void Binary()
+{
+    int count = 0; // кол-во прохождений цикла while
+    int temp = number;
+
+    while (number != 0) // пока НЕ равно нулю
+    {
+        number /= 2;
+        count++;
+    }
+    int[] BinArray = new int[count];
+
+    count--;
+    while (temp != 0)
+    {
+        BinArray[count] = temp % 2;
+        temp /= 2;
+        count--;
+    }
+    Console.Write($"Число {value} в двоичной системе: " + String.Join(" ", BinArray));
+}
+Binary();
+
+Console.WriteLine();
+
+void Binary2()
+{
+    string binary = Convert.ToString(value, 2);
+    Console.WriteLine($"Число {value} в двоичной системе: {binary}");
+}
+Binary2();
