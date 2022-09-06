7b4a545dfdaa6d2b6aa53ae8e7e7f75716802c5d
Victoria <victoriaerygina@yandex.ru>
2022-09-06 16:09:20 +0700

    Копия массива

---
+++ Задача_45/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+Console.Write("Введите длину массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 11);
+}
+Console.WriteLine($"Массив: [{String.Join(", ", array)}]");
+
+int[] copyArray = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    copyArray[i] = array[i];
+}
+Console.WriteLine($"Копия: [{String.Join(", ", copyArray)}]");
